928af18972056ad498cbfd72ba147ee4069b62a5
petrushka35 <61370310+petrushka35@users.noreply.github.com>
2020-10-27 16:56:04 +0700

    problem 26

--- olimpiade.cpp
+++ olimpiade.cpp
@@ -6,12 +6,16 @@ using namespace std;
 
 int main()
 {
-	int x1, x2, y1, y2, answer;
+	int x1, x2, y1, y2, r1, r2, answer;
 	
-	cin >> x1 >> y1 >> x2 >> y2;
+	cin >> x1 >> y1 >> r1;
+	cin >> x2 >> y2 >> r2;
 
 	answer = sqrt(unsigned(x2 - x1) * unsigned(x2 - x1) + unsigned(y2 - y1) * unsigned(y2 - y1));
-	cout << answer << endl;
+	if (r1 + r2 > answer)
+		cout << "YES" << endl;
+	else
+		cout << "NO" << endl;
 
 	system("pause");
 	return 0;
